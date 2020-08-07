42d5ea2c90a2e0fc77a69d65c69578e4645515d8
尉勇强2 <hzweiyongqiang@corp.netease.com>
2020-08-07 16:40:10 +0800

    update_Y-d~M:

--- start-push-git.cmd
+++ start-push-git.cmd
@@ -1,6 +1,6 @@
-backtime=`date +%Y-%m-%d~%H:%M:%S`
+set backtime=`date +%Y-%m-%d~%H:%M:%S`
 git add .
-git commit -m update_%backtime%
+git commit -m update_%date +%Y-%m-%d~%H:%M:%S%
 git push origin master
 
 pause
